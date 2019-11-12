d7a7bb373ebdf0e656d37eddaf1a3bc14a41d596
Brian <wilsonedit@gmail.com>
2019-11-12 07:57:53 -0600

    Initial commit, reflects classroon instruction only

---
+++ StudentExercisesMVC/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudentExercisesMVC.Models;
+
+namespace StudentExercisesMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var students = new List<Student>()
+            {
+                new Student()
+                {
+                    FirstName = "Roy",
+                    LastName = "Batty",
+                },
+                  new Student()
+                {
+                    FirstName = "Tony",
+                    LastName = "Danza",
+                }
+            };
+            return View(students);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StudentExercisesMVC/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Slack")]
+        public string SlackHandle { get; set; }
+        public int CohortId {get; set;}
+    }
+}
